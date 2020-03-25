65ad141f2ae2a12f8b5e814b200ca90b82e34248
pranaliandre <pranaliandre06@gmail.com>
2020-03-25 15:08:14 -0500

    Added: Check gambler win or loss

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -1,7 +1,19 @@
 #!/bin/bash -x
 echo "welcome to Gambling Simulation"
-#variable
+#constant variable
 BET=1
 EVERY_DAY_STAKE=100
-
-
+#variable
+RS=$EVERY_DAY_STAKE
+#function to check win or loss
+function gamblerWinLoss(){
+		if [ $((RANDOM%2)) -eq 1 ]
+		then
+			RS=$((RS+BET))
+			echo "win"
+		else
+			RS=$((RS-BET))
+			echo "loss"
+		fi			
+}
+gamblerWinLoss
