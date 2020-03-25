91855e132c6d4ea2d3a7fb79b4c181b5ec4cfee0
pranaliandre <pranaliandre06@gmail.com>
2020-03-25 15:33:04 -0500

    Added: calculating gambler if won or lost 50% of the stake daily limit

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -5,8 +5,14 @@ BET=1
 EVERY_DAY_STAKE=100
 #variable
 RS=$EVERY_DAY_STAKE
+
+PERCENTAGE_LOWER=$(($EVERY_DAY_STAKE-$(($EVERY_DAY_STAKE*50/100)) ))
+PERCENTAGE_UPPER=$(($EVERY_DAY_STAKE+$(($EVERY_DAY_STAKE*50/100)) ))
+
 #function to check win or loss
 function gamblerWinLoss(){
+	while [ $RS -gt $PERCENTAGE_LOWER ] && [ $RS -lt $PERCENTAGE_UPPER ]
+	do
 		if [ $((RANDOM%2)) -eq 1 ]
 		then
 			RS=$((RS+BET))
@@ -15,5 +21,6 @@ function gamblerWinLoss(){
 			RS=$((RS-BET))
 			echo "loss"
 		fi			
+	done
 }
 gamblerWinLoss
